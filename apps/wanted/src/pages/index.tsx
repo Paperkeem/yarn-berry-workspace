933f4d7b970489010f14fd993bc4ed376cf069ce
Paperkeem <ahharlawhddl@gmail.com>
2023-05-10 00:35:44 +0900

    feat: typecheck

--- apps/wanted/src/pages/index.tsx
+++ apps/wanted/src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         </p>
         <code>{sayHello()}</code>
         <br></br>
-        <Button>Hello paper!</Button>
+        <Button variant="contained">Hello paper!</Button>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
